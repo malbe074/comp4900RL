efcd85eb4b04e91a2f96535b703bbbc76d55fdef
shaunsim15 <shaunsim_15@hotmail.com>
2023-12-09 01:05:26 -0500

    Minor tweak

--- working-folder/graph_plotting.py
+++ working-folder/graph_plotting.py
@@ -25,7 +25,7 @@ def plot_experiment(experimentParameter):
         # CHANGE the range of the file name to only specify the variable number in your file name
         # E.g. if fileName is Alpha5e-05.csv, then str(fname[7:12]) will return 5e-05
         plotColor.append("$"+experimentParameter+"=" +
-                         str(fname[3:]).rstrip('.csv')+"$")
+                         str(fname[2:]).rstrip('.csv')+"$")
 
     plt.legend(plots, plotColor)
     plt.show()
